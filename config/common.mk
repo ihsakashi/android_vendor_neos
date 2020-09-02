eb0d0d363248e88685b96141925c30ba1db1d397
ihsakashi <yuvch122@gmail.com>
2020-09-02 17:45:56 -0600

    cleanup

--- config/common.mk
+++ config/common.mk
@@ -1,18 +1,18 @@
 # NEOS Vendor
 
-# AOSPA Specific
-TARGET_DISABLES_GAPPS := true
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/neos/overlay/common
 
 # Termux usr
 #$(info $(shell python $(vendor/neos/termux_usr/install.py)))
 
+$(shell mkdir -p $(TARGET_COPY_OUT_SYSTEM)/neos/)
+$(shell mkdir -p $(TARGET_OUT_ETC)/ssh/)
+#$(shell cp -r vendor/neos/prebuilt/neos/home $(TARGET_COPY_OUT_SYSTEM)/neos/)
+#$(shell cp -r vendor/neos/termux_usr/out/data/data/com.termux/files/usr $(TARGET_COPY_OUT_SYSTEM)/neos/)
+
 # Termux neos env
 PRODUCT_COPY_FILES += \
-#	$(call find-copy-subdir-files,*,vendor/neos/termux_usr/out/data/data/com.termux/files/usr,$(TARGET_COPY_OUT_SYSTEM)/neos/usr) \
-#	$(call find-copy-subdir-files,*,vendor/neos/prebuilt/neos/home,$(TARGET_COPY_OUT_SYSTEM)/neos/home) \
 	$(call find-copy-subdir-files,*,vendor/neos/prebuilt/common/etc/ssh,$(TARGET_COPY_OUT_SYSTEM)/etc/ssh)
 
 # Neos rc and scripts
