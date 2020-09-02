4d9b755e5bd40f0fb5bea7598b4f52880afa1803
ihsakashi <yuvch122@gmail.com>
2020-09-01 22:37:51 -0600

    copy ssh keys and config

--- config/common.mk
+++ config/common.mk
@@ -9,11 +9,13 @@ DEVICE_PACKAGE_OVERLAYS += vendor/neos/overlay/common
 # Termux usr
 #$(info $(shell python $(vendor/neos/termux_usr/install.py)))
 
+# Termux comma env
 PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,vendor/neos/termux_usr/out/data/data/com.termux/files/usr,system/comma/usr) \
 	$(call find-copy-subdir-files,*,vendor/neos/prebuilt/comma/home,system/comma/home) \
+	$(call find-copy-subdir-files,*,vendor/neos/prebuilt/common/etc/ssh,system/etc/ssh)
 
-# neos rc and scripts
+# Neos rc and scripts
 PRODUCT_PACKAGES += \
 	neos-common.rc \
 	neos.sh \
