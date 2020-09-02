b3279f538ccc77338f6ca802b36151bdc4d0ad3f
ihsakashi <yuvch122@gmail.com>
2020-09-01 22:11:32 -0600

    include neos rc and scripts

--- config/common.mk
+++ config/common.mk
@@ -6,20 +6,18 @@ TARGET_DISABLES_GAPPS := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/neos/overlay/common
 
-# Copy all NEOS-specific init rc files
-$(foreach f,$(wildcard vendor/neos/prebuilt/common/etc/init/*.rc),\
-	$(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
-
-# Copy all NEOS-specific sh files
-$(foreach f,$(wildcard vendor/neos/prebuilt/common/etc/*.sh),\
-	$(eval PRODUCT_COPY_FILES += $(f):system/etc/$(notdir $f)))
-
 # Termux usr
 #$(info $(shell python $(vendor/neos/termux_usr/install.py)))
 
 PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,vendor/neos/termux_usr/out/data/data/com.termux/files/usr,system/comma/usr) \
 	$(call find-copy-subdir-files,*,vendor/neos/prebuilt/comma/home,system/comma/home) \
+
+# neos rc and scripts
+PRODUCT_PACKAGES += \
+	neos-common.rc \
+	neos.sh \
+	neos-ssh.sh
 
 # Window Wrapper Lib
 PRODUCT_PACKAGES += \
