a4968ab25f1064f624e404584d0d6b76a101f6dd
ihsakashi <yuvch122@gmail.com>
2020-09-03 20:10:05 -0600

    hax way shell dont work :(

--- config/common.mk
+++ config/common.mk
@@ -6,19 +6,20 @@ DEVICE_PACKAGE_OVERLAYS += vendor/neos/overlay/common
 # Termux usr
 #$(info $(shell python $(vendor/neos/termux_usr/install.py)))
 
-$(shell mkdir -p $(TARGET_OUT)/neos/)
-$(shell cp -r vendor/neos/prebuilt/neos/home $(TARGET_OUT)/neos/)
-$(shell cp -r vendor/neos/termux_usr/out/data/data/com.termux/files/usr $(TARGET_OUT)/neos/)
-
-# Termux neos env
+# Termux env
 PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,vendor/neos/prebuilt/common/etc/ssh,$(TARGET_COPY_OUT_SYSTEM)/etc/ssh)
+	$(call find-copy-subdir-files,*,vendor/neos/prebuilt/neos/home,$(TARGET_COPY_OUT_SYSTEM)/neos/home)
+	$(call find-copy-subdir-files,*,vendor/neos/termux_usr/out/data/data/com.termux/files/usr,$(TARGET_COPY_OUT_SYSTEM)/neos/usr)
 
 # Neos rc and scripts
 PRODUCT_PACKAGES += \
 	neos-common.rc \
 	neos-main \
 	neos-ssh
+
+# ssh
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,vendor/neos/prebuilt/common/etc/ssh,$(TARGET_COPY_OUT_SYSTEM)/etc/ssh)
 
 # ssh system
 PRODUCT_PACKAGES += \
