f8eb0b800ae820798c62d577ef814d71b2264cd8
ihsakashi <yuvch122@gmail.com>
2020-09-04 20:02:59 -0600

    fix? and easier

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -6,14 +6,9 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/ssh
 
-LOCAL_SRC_FILES := \
-	ssh_host_dsa_key \
-	ssh_host_dsa_key.pub \
-	ssh_host_ecdsa_key \
-	ssh_host_ecdsa_key.pub \
-	ssh_host_ed25519_key \
-	ssh_host_ed25519_key.pub \
-	ssh_host_rsa_key \
-	ssh_host_rsa_key.pub
+KEY_FILES := $(wildcard $(LOCAL_PATH)/etc/ssh/*)
+KEY_FILES := $(KEY_FILES:$(LOCAL_PATH)/%=%)
+
+LOCAL_SRC_FILES := $(PROJECT_FILES)
 
 include $(BUILD_PREBUILT)
