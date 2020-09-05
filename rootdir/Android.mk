1cfff4117112f5c0c6e1c23cb9ca7e35c29e2a21
ihsakashi <yuvch122@gmail.com>
2020-09-04 19:19:20 -0600

    forgot neos-setup and redo dir

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,8 +4,16 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= neos-common.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/neos-common.rc
+LOCAL_SRC_FILES		:= etc/init/neos-common.rc
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= neos-setup
+LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= bin/neos-setup
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
