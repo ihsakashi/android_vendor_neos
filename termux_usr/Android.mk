ba6771f1a8e3289a3966741c63dc452294510ee1
ihsakashi <yuvch122@gmail.com>
2020-09-05 15:07:38 -0600

    bootstrap is what tis

--- termux_usr/Android.mk
+++ termux_usr/Android.mk
@@ -5,18 +5,18 @@ LOCAL_MODULE := termux_usr
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 
-MY_SCRIPT := vendor/neos/termux_usr/install.py
+MY_SCRIPT := vendor/neos/termux_usr/generate-bootstraps.sh
 
 working:= $(local-intermediates-dir)
 
-FINAL_TAR_FILE := $(local-generated-sources-dir)/termux_usr.tar
+FINAL_TAR_FILE := $(local-generated-sources-dir)/bootstrap.zip
 $(FINAL_TAR_FILE): PRIVATE_SCRIPT := $(MY_SCRIPT)
 $(FINAL_TAR_FILE):
 	rm -f $@
 	mkdir -p $(working)
-	cp -f $(PRIVATE_SCRIPT) $(working)
-	/usr/bin/python $(working)/install.py
-	tar -cvf $@ $(working)/out/data/data/com.termux/files/usr
+	cd $(working)
+	$(PRIVATE_SCRIPT) --architecture aarch64 --add autoconf,automake,bison,clang,cmake,coreutils,curl,ffmpeg,flex,gdb,git,git-lfs,htop,jq,libcurl-static,libffi-static,libjpeg-turbo,libjpeg-turbo-static,liblz4,liblz4-static,liblzo,liblzo-static,libmpc,libtool,libuuid-static,libzmq,libpcap,libpcap-static,libpng,make,man,nano,ncurses-static,patchelf,python-static,strace,tar,tmux,vim,wget,zlib-static,zsh
+	mv -f bootstrap--aarch64.zip $@
 
 LOCAL_PREBUILT_MODULE_FILE := $(FINAL_TAR_FILE)
 
