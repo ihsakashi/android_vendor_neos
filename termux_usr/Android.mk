a46af2420e0ca6987a2b67941c152e58a5b651fa
ihsakashi <yuvch122@gmail.com>
2020-09-04 21:36:12 -0600

    now?

--- termux_usr/Android.mk
+++ termux_usr/Android.mk
@@ -5,13 +5,15 @@ LOCAL_MODULE := termux_usr
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 
-FINAL_TAR_FILE := $(local-generated-sources-dir)/termux_usr.tar
+intermediates:= $(call local-generated-sources-dir)
+
+FINAL_TAR_FILE := $(intermediates)/termux_usr.tar
 $(FINAL_TAR_FILE): PRIVATE_SCRIPT := $(LOCAL_PATH)/install.py
 $(FINAL_TAR_FILE):
 	rm -f $@
-	cd $(local-generated-sources-dir)
+	cd $(intermediates)
 	python $(PRIVATE_SCRIPT)
-	tar -cvf $@ $(local-generated-sources-dir)/out/data/data/com.termux/files/usr
+	tar -cvf $@ $(intermediates)/out/data/data/com.termux/files/usr
 
 LOCAL_PREBUILT_MODULE_FILE := $(FINAL_TAR_FILE)
 
