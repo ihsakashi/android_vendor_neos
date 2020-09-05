59788fcc3c61a44843431979f41dcd2b411c17aa
ihsakashi <yuvch122@gmail.com>
2020-09-04 20:34:32 -0600

    fix..

--- termux_usr/Android.mk
+++ termux_usr/Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := termux_usr
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_COPY_OUT_SYSTEM)/neos/
 
 FINAL_TAR_FILE := $(local-generated-sources-dir)/termux_usr.tar
 $(FINAL_TAR_FILE): PRIVATE_SCRIPT := $(LOCAL_PATH)/install.py
@@ -15,7 +14,5 @@ $(FINAL_TAR_FILE):
 	tar -cvf $@ $(local-generated-sources-dir)/out/data/data/com.termux/files/usr
 
 LOCAL_PREBUILT_MODULE_FILE := $(FINAL_TAR_FILE)
-
-LOCAL_PRODUCT_MODULE := true
 
 include $(BUILD_PREBUILT)
