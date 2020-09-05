05ba0569ebf97ce30f4a958e6830095818913baa
ihsakashi <yuvch122@gmail.com>
2020-09-04 19:48:42 -0600

    fix

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -7,13 +7,13 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/ssh
 
 LOCAL_SRC_FILES := \
-	ssh_host_dsa_key
-	ssh_host_dsa_key.pub
-	ssh_host_ecdsa_key
-	ssh_host_ecdsa_key.pub
-	ssh_host_ed25519_key
-	ssh_host_ed25519_key.pub
-	ssh_host_rsa_key
+	ssh_host_dsa_key \
+	ssh_host_dsa_key.pub \
+	ssh_host_ecdsa_key \
+	ssh_host_ecdsa_key.pub \
+	ssh_host_ed25519_key \
+	ssh_host_ed25519_key.pub \
+	ssh_host_rsa_key \
 	ssh_host_rsa_key.pub
 
 include $(BUILD_PREBUILT)
