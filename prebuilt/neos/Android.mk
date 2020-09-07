f2ac5dc3ab0d606c79ac782b878a29976e736455
ihsakashi <yuvch122@gmail.com>
2020-09-06 19:12:58 -0600

    final rev -zip home cuz tar preserves -fix symlink -fix ssh ish -fix comma dir -add tsu

--- prebuilt/neos/Android.mk
+++ prebuilt/neos/Android.mk
@@ -1,15 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := termux_home.tar
+LOCAL_MODULE := termux-home.zip
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 
-FINAL_TAR_FILE := $(local-generated-sources-dir)/termux_home.tar
-$(FINAL_TAR_FILE): PRIVATE_INPUT_DIR := $(LOCAL_PATH)
+FINAL_TAR_FILE := $(local-generated-sources-dir)/termux-home.zip
+$(FINAL_TAR_FILE): PRIVATE_INPUT_DIR := $(LOCAL_PATH)/home
 $(FINAL_TAR_FILE):
 	rm -f $@
-	tar -cvf $@ -C $(PRIVATE_INPUT_DIR) ./home
+	cd $(PRIVATE_INPUT_DIR)
+	zip -r9 $@ ./*
 
 LOCAL_PREBUILT_MODULE_FILE := $(FINAL_TAR_FILE)
 
