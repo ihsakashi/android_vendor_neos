868d1544834c67e9e14c1cc9c30b2b218f3d505d
ihsakashi <yuvch122@gmail.com>
2020-09-07 00:03:49 -0600

    wertf

--- prebuilt/neos/Android.mk
+++ prebuilt/neos/Android.mk
@@ -9,7 +9,7 @@ FINAL_TAR_FILE := $(local-generated-sources-dir)/termux-home.zip
 $(FINAL_TAR_FILE): PRIVATE_INPUT_DIR := $(LOCAL_PATH)/home
 $(FINAL_TAR_FILE):
 	rm -f $@
-	(cd $(PRIVATE_INPUT_DIR) && zip -r9 $@ ./*)
+	(cd $(PRIVATE_INPUT_DIR) && zip -r $@ ./*)
 
 LOCAL_PREBUILT_MODULE_FILE := $(FINAL_TAR_FILE)
 
